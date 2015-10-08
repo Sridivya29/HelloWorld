ee9155c94c8f91c2ff229a264e6833814c46884e
Sridivya29 <Sridivya29@users.noreply.github.com>
2015-10-08 09:49:46 -0400

    HelloWorld

---
+++ HelloWorldWebAPI/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HelloWorldWebAPI.Controllers
+{
+    public class HelloWorldController : ApiController
+    {
+        // GET api/HelloWorld
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
